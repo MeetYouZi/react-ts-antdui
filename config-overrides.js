955cca90da0b9dee9378fe4fbb4c331601651da7
meetyouzi <987176347@qq.com>
2020-07-21 23:55:03 +0800

    完善 Button 组件

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,5 @@ const path = require('path')
 module.exports = override(
   addWebpackAlias({
     '@': path.resolve(__dirname, 'src'),
-    'style': path.resolve(__dirname, 'src/style'),
-    'components': path.resolve(__dirname, 'src/components')
   }),
 )
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {AnchorHTMLAttributes, ButtonHTMLAttributes} from 'react'
 import classNames from 'classnames'
 
 // enum 枚举类型
@@ -23,18 +23,27 @@ interface BaseButtonProps {
   href?: string
 }
 
-const Button: React.FC<BaseButtonProps> = (props) => {
+// 1,type 类型别名  2,React.ButtonHTMLAttributes 返回新 Button 属性
+// A | B 联合类型 返回 A 或者 B ；A & B 交叉类型 多个类型叠加新的类型，包含了所属的所有的特性
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+// Partial 属性可以把上面的属性都设置成可选的
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const Button: React.FC<ButtonProps> = (props) => {
 
   const {
     btnType,
+    className,
     disabled,
     size,
     children,
-    href
+    href,
+    ...restProps
   } = props
 
   // btn, btn-lg, btn-primary
-  const classes = classNames('btn', {
+  const classes = classNames('btn', className, {
     // 如果 btnType 存在就添加 btn-xxx class
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
@@ -46,6 +55,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
       <a
         className={classes}
         href={href}
+        {...restProps}
       >
         {children}
       </a>
@@ -55,6 +65,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
       <button
         className={classes}
         disabled={disabled}
+        {...restProps}
       >
         {children}
       </button>
